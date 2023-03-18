3e8532c644ede6107b0b61dd2b17178f6339bff8
Dionei Bianchati <dion.biancha@gmail.com>
2023-03-18 10:15:25 -0300

    feat: animações do player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,13 @@ public class Player : MonoBehaviour
     public bool doubleJump;
 
     private Rigidbody2D rig;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +30,21 @@ public class Player : MonoBehaviour
     void Move(){
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f) {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f) {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+          if(Input.GetAxis("Horizontal") == 0f) {
+            anim.SetBool("walk", false);
+        }
+        
     }
 
     void Jump(){
@@ -35,10 +52,12 @@ public class Player : MonoBehaviour
             if(!isJumping){
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump", true);
             }else {
                 if(doubleJump){
                      rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                      doubleJump = false;
+                     
                 }
             }
             
@@ -48,6 +67,7 @@ public class Player : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.layer == 8){
             isJumping = false;
+            anim.SetBool("jump", false);
         }
     }
 
